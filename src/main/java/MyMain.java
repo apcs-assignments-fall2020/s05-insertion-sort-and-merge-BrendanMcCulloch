8b0679bd2a267a49d0045ee328484cd3ac585080
BrendanMcCulloch <22bmcculloch@athenian.org>
2021-02-10 07:02:50 +0000

    method two doesnt work

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,16 +4,44 @@ public class MyMain {
 
     // Sorts the ArrayList using insertion sort
     public static ArrayList<Integer> insertionSort(ArrayList<Integer> list) {
-        // YOUR CODE HERE
-        return null;
+        for (int i = 1; i < list.size(); i++){
+            int b = list.get(i);
+            int c = i - 1;
+            while ((c > -1) && (list.get(c) > b )){
+                list.set(c+1, list.get(c));
+                c -= 1;
+            }
+            list.set(c + 1, b);
+        }
+        return list;
     }
 
      // Merges two sorted arrays into one large combined
      // sorted array
      // You may assume arr1 and arr2 are the same length
     public static int[] merge(int[] arr1, int[] arr2) { 
-        // YOUR CODE HERE
-        return null;
+        int c = 0;
+        int b = 0;
+        int t = 0;
+        int[] oa = new int[arr1.length + arr2.length];
+        while(arr2[b] < arr1[c]){
+            arr2[b] = oa[t];
+            b++;
+            t++;
+            if(arr2[b] > arr1[c]){
+                break;
+            }
+        }
+        while(arr2[b] > arr1[c]){
+            arr1[c] = oa[t];
+            c++;
+            t++;
+            if(arr2[b] < arr1[c]){
+                break;
+            }
+        }   
+        return oa;        
+     
     }
 
     public static void main(String[] args) {
